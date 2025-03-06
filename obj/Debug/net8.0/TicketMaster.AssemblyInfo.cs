9328c39110e39bd94322bfc8ebcbe23619c70de7
AtorVP64 <attika.szakal@gmail.com>
2025-03-06 10:38:31 +0100

    ???

--- obj/Debug/net8.0/TicketMaster.AssemblyInfo.cs
+++ obj/Debug/net8.0/TicketMaster.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketMaster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e03e6f89b9d6c8cffb32bda735514136d2bdcd41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09834b329fe283fa535122a57556ed3fb7f270e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketMaster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketMaster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
