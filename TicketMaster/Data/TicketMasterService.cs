9a3569cabbeeda5046c6a6ff0f0fbe0ccc5b2fe0
AtorVP64 <attika.szakal@gmail.com>
2025-04-05 13:46:25 +0200

    We now have images fpr the movies

--- TicketMaster/Data/TicketMasterService.cs
+++ TicketMaster/Data/TicketMasterService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 using TicketMaster.Objects;
 
 namespace TicketMaster.Data
@@ -12,6 +14,31 @@ namespace TicketMaster.Data
                     .Select(movie => movie)
                     .Where(movie => movie.Id < last && movie.Id >= first)
                     .ToList();
+        }
+
+        public static string OmdbApi(int imdbID, string APIkey) => 
+            $"http://www.omdbapi.com/?i=tt{imdbID:D7}&apikey={APIkey}&";
+
+        public static async IAsyncEnumerable<string> ImageSrcLinkAsync(Movie movie)
+        {
+            while (true)
+            {
+                string apiResult;
+                using var httpClient = new HttpClient();
+                apiResult = await httpClient.GetStringAsync(OmdbApi(movie.ImdbId, "3a5dfbb5"));
+                JsonDocument apiJson = JsonDocument.Parse(apiResult);
+                if (apiJson.RootElement.TryGetProperty("Poster", out JsonElement result))
+                {
+                    yield return result.GetString();
+                    yield break;
+                }
+                else
+                {
+                    Console.WriteLine("Poster key is missing");
+                    yield return null;
+                }
+                await Task.Delay(500);
+            }
         }
     }
 }
