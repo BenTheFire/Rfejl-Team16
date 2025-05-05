890bdc8c5226def6b146c88c0c05f3b2b81503d5
AtorVP64 <attika.szakal@gmail.com>
2025-05-05 15:02:19 +0200

    Password = HASHED

--- TicketMaster/Data/Services/Implementations/LoginService.cs
+++ TicketMaster/Data/Services/Implementations/LoginService.cs
@@ -1,4 +1,5 @@
-﻿using Org.BouncyCastle.Pqc.Crypto.Lms;
+﻿using Microsoft.AspNetCore.Identity;
+using Org.BouncyCastle.Pqc.Crypto.Lms;
 using TicketMaster.Data.DTOs;
 using TicketMaster.Data.Services.Interfaces;
 using TicketMaster.Extra;
@@ -21,18 +22,16 @@ namespace TicketMaster.Data.Services.Implementations
             isAdmin = false;
             if (Model.Username.IsNullOrEmpty() && Model.Password.IsNullOrEmpty())
             {
-                if (_context.Users.Any(o => o.Username.CompareTo(Model.Username) == 0))
+                var user = _context.Users.Where(o => o.Username == Model.Username).First();
+                username = user.Username;
+                password = user.PasswordHash;
+                if (!_context.Users.Any(o => o.Username.CompareTo(Model.Username) == 0))
                 {
-                    username = _context.Users.Where(o => o.Username == Model.Username).Select(o => o.Username).First();
-                    password = _context.Users.Where(o => o.Username == Model.Username).Select(o => o.PasswordHash).First();
-                }
-                else
-                {
-                    username = _context.Administrators.Where(o => o.Name == Model.Username).Select(o => o.Name).First();
-                    password = _context.Administrators.Where(o => o.Name == Model.Username).Select(o => o.PasswordHash).First();
                     isAdmin = true;
                 }
-                return username.Equals(Model.Username) && password.Equals(Model.Password);
+                return username.Equals(Model.Username) && 
+                    new PasswordHasher<string>().HashPassword(Model.Username, Model.Password)
+                        .Equals(password);
             }
             else
             {
