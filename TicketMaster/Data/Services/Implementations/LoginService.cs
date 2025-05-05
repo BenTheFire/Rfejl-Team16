b3a35e6baa5c971f0a00e23db92bf645e876d3f0
AtorVP64 <attika.szakal@gmail.com>
2025-05-05 15:07:46 +0200

    spaced

--- TicketMaster/Data/Services/Implementations/LoginService.cs
+++ TicketMaster/Data/Services/Implementations/LoginService.cs
@@ -22,7 +22,9 @@ namespace TicketMaster.Data.Services.Implementations
             var user = _context.Users.Where(o => o.Username == Model.Username).First();
             string username = user.Username;
             string passwordHash = user.PasswordHash;
+
             isAdmin = !_context.Users.Any(o => o.Username.CompareTo(Model.Username) == 0) ? true : false;
+
             return username.Equals(Model.Username) &&
                 new PasswordHasher<string>().HashPassword(Model.Username, Model.Password)
                     .Equals(passwordHash);
