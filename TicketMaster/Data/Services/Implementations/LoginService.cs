e1d879c4b579ebcf150349e6f5da0d46e2d68746
AtorVP64 <attika.szakal@gmail.com>
2025-05-05 15:07:25 +0200

    Code cleaned up

--- TicketMaster/Data/Services/Implementations/LoginService.cs
+++ TicketMaster/Data/Services/Implementations/LoginService.cs
@@ -3,7 +3,6 @@ using TicketMaster.Data.DTOs;
 using TicketMaster.Data.Services.Interfaces;
 using TicketMaster.Extra;
 using TicketMaster.Objects;
-using TicketMaster.Objects.Users;
 
 namespace TicketMaster.Data.Services.Implementations
 {
@@ -16,26 +15,17 @@ namespace TicketMaster.Data.Services.Implementations
         private readonly TicketmasterContext _context;
         public bool LoginUser(ref bool isAdmin, LoginUserDTO Model)
         {
-            string username;
-            string password;
-            isAdmin = false;
-            if (Model.Username.IsNullOrEmpty() && Model.Password.IsNullOrEmpty())
-            {
-                var user = _context.Users.Where(o => o.Username == Model.Username).First();
-                username = user.Username;
-                password = user.PasswordHash;
-                if (!_context.Users.Any(o => o.Username.CompareTo(Model.Username) == 0))
-                {
-                    isAdmin = true;
-                }
-                return username.Equals(Model.Username) && 
-                    new PasswordHasher<string>().HashPassword(Model.Username, Model.Password)
-                        .Equals(password);
-            }
-            else
-            {
+            if (!Model.Username.IsNullOrEmpty() || !Model.Password.IsNullOrEmpty())
                 return false;
-            }
+
+
+            var user = _context.Users.Where(o => o.Username == Model.Username).First();
+            string username = user.Username;
+            string passwordHash = user.PasswordHash;
+            isAdmin = !_context.Users.Any(o => o.Username.CompareTo(Model.Username) == 0) ? true : false;
+            return username.Equals(Model.Username) &&
+                new PasswordHasher<string>().HashPassword(Model.Username, Model.Password)
+                    .Equals(passwordHash);
         }
     }
 }
