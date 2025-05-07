04468f07363a4abcad29caebb71b4e7f4e24cff2
Hellie Prankster <hellie.personal@gmail.com>
2025-05-07 15:49:30 +0200

    now its updated

---
+++ TicketMaster/Migrations/MySql/20250507134910_Guh5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketMaster.Migrations.MySql
+{
+    /// <inheritdoc />
+    public partial class Guh5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PurchaseTime",
+                table: "Tickets",
+                type: "datetime(6)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PurchaseTime",
+                table: "Tickets");
+        }
+    }
+}
