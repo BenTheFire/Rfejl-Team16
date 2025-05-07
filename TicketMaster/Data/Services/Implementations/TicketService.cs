b5142800ef6f3bc33420c0bde1c857ca0e2ccd97
AtorVP64 <attika.szakal@gmail.com>
2025-05-07 15:45:46 +0200

    TicketService

---
+++ TicketMaster/Data/Services/Implementations/TicketService.cs
@@ -0,0 +1,46 @@
+﻿using TicketMaster.Data.Services.Interfaces;
+using TicketMaster.Objects;
+
+namespace TicketMaster.Data.Services.Implementations
+{
+    public class TicketService : ITicketService
+    {
+        private readonly TicketmasterContext _context;
+        public TicketService(TicketmasterContext c)
+        {
+            _context = c;
+        }
+
+        public List<Ticket> FetchOwnedTickets(string username)
+        {
+            var ret = (
+                from u in _context.Users
+                join c in _context.CustomerData on u equals c.OfUser
+                join t in _context.Tickets on c equals t.Customer
+                where u.Username == username && t.Status == 1 // approved
+                select t).ToList();
+            return ret;
+        }
+
+        public List<Ticket> FetchPendingTickets(string vendorname)
+        {
+            var ret = (
+                from v in _context.Vendors
+                join t in _context.Tickets on v equals t.ByVendor
+                where v.Username == vendorname && t.Status == 0 // pending
+                select t).ToList();
+            return ret;
+        }
+
+        public List<Ticket> FetchPurchases(string username)
+        {
+            var ret = (
+                from u in _context.Users
+                join c in _context.CustomerData on u equals c.OfUser
+                join t in _context.Tickets on c equals t.Customer
+                where u.Username == username
+                select t).ToList();
+            return ret;
+        }
+    }
+}
---
+++ TicketMaster/Data/Services/Interfaces/ITicketService.cs
@@ -0,0 +1,11 @@
+﻿using TicketMaster.Objects;
+
+namespace TicketMaster.Data.Services.Interfaces
+{
+    public interface ITicketService
+    {
+        public List<Ticket> FetchPurchases(string username);
+        public List<Ticket> FetchPendingTickets(string vendorUsername);
+        public List<Ticket> FetchOwnedTickets(string username);
+    }
+}
