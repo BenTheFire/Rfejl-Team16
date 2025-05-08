3db6c2777f5620fcf441dbef71ee3cfc70b7fe35
AtorVP64 <attika.szakal@gmail.com>
2025-05-08 12:16:42 +0200

    Try it now

--- TicketMaster/Objects/Administrator.cs
+++ TicketMaster/Objects/Administrator.cs
@@ -4,5 +4,6 @@ namespace TicketMaster.Objects
 {
     public class Administrator : AuthenticateUser
     {
+        public new int Id { get => int.Parse(base.Id); set => base.Id = value.ToString(); }
     }
 }
