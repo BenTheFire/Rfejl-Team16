77f8e4f8ae6edc15b1bcddaebe725b76f140644f
AtorVP64 <attika.szakal@gmail.com>
2025-05-08 13:44:10 +0200

    Reapply "Try it now"
    This reverts commit 09149fb378e8d69c0e8898fb103b628379affef9.

--- TicketMaster/Objects/Vendor.cs
+++ TicketMaster/Objects/Vendor.cs
@@ -5,6 +5,7 @@ namespace TicketMaster.Objects
 {
     public class Vendor : AuthenticateUser
     {
+        public new int Id { get => int.Parse(base.Id); set => base.Id = value.ToString(); }
         public DbSet<Location> Locations { get; set; }
     }
 }
